d872f6cd52411cdb911895305c0cbb292e47560d
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-13 03:58:34 -0500

    Variables en Python

---
+++ Conceptos Básicos/2.-Variables.py
@@ -0,0 +1,6 @@
+CONSTANTE = "Al no existir constantes en Python, se recomienda poner el nombre de la variable en mayúsculas"
+
+nombre_tutor = "Cody"
+
+print("Imprimiendo una variable: " + nombre_tutor)
+print(CONSTANTE)
