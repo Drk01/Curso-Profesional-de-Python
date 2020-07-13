21b5e2bad103e64b954e24e1bfb085d386bf1b70
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-13 04:18:58 -0500

    Comentarios en Python

---
+++ Conceptos Básicos/6.- Comentarios.py
@@ -0,0 +1,7 @@
+# Este es un comentario de una línea
+
+""" 
+Este es un comentario de tipo párrafo.
+Como puedes ver, es muy sencillo de hacer.
+Solo tienes que poner triples comillas dobles y dejar volar tu imaginación
+"""
