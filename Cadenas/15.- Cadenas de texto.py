4bd44078c56c0b0e8a1fa92b02cd7e3adc31cee2
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-13 16:11:49 -0500

    Cadenas de caracteres en Python

---
+++ Cadenas/15.- Cadenas de texto.py
@@ -0,0 +1,13 @@
+curso = "Curso de Python 3"
+
+# Obtener la cantidad de caracteres del string
+resultado = len(curso)
+print(resultado)
+
+# Obtener la primera letra
+resultado = curso[0]
+print(resultado)
+
+# Generando substrings
+resultado = curso[1:16:2]
+print(resultado)
