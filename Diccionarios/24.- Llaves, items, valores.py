5de70f3a7dfc1d4e62e8df0d36d2d63bf62f1e01
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-14 01:05:30 -0500

    Llaves, items y valores en Python

---
+++ Diccionarios/24.- Llaves, items, valores.py
@@ -0,0 +1,12 @@
+diccionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+
+# Obteniendo las llaves
+resultado = diccionario.keys()
+
+# Obteniendo los valores
+resultado = diccionario.values()
+
+# Obteniendo las llaves y valores
+resultado = diccionario.items()
+
+print(resultado)
