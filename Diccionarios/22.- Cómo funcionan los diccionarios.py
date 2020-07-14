d2361c1a30995d648da38c5a08e66ba3be0602c2
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-13 22:07:54 -0500

    ¿Cómo funcionan los diccionarios en Python?

---
+++ Diccionarios/22.- Cómo funcionan los diccionarios.py
@@ -0,0 +1,10 @@
+diccionario = {}
+
+diccionario['nombre'] = 'Coddy'  # Agregar una llave con su valor
+
+valor = diccionario['nombre']  # Obtener un valor de un diccionario
+
+diccionario['nombre'] = 90  # Cambio de valor en el diccionario
+
+print(diccionario)
+print(valor)
