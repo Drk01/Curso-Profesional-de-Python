feda278584cbb010f37c86bbff1c99ae76fc251b
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-14 20:58:56 -0500

    Función range y enumerate en Python

---
+++ Ciclos y Condicionales/30.- Función range y enumerate.py
@@ -0,0 +1,9 @@
+# Función range
+lista = [1, 2, 3, 4, 5, 6]
+
+for valor in range(1, 101, 2):
+    print(valor)
+
+# Función enumerate
+for indice, valor in enumerate(lista):
+    print('indice: ', indice, 'valor: ', valor)
