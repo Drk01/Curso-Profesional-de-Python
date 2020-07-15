7660d062a9949bb09cee25c3eb1389bf93d970f5
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-15 02:31:09 -0500

    Break y Continue en Python

---
+++ Ciclos y Condicionales/31.- Break y Continue.py
@@ -0,0 +1,9 @@
+titulo = "Curso de Python 3"
+
+for caracter in titulo:
+    if caracter == 'P':
+        continue
+    print(caracter)
+
+# break: Rompe el ciclo
+# continue: Saltamos a la siguiente iteración
