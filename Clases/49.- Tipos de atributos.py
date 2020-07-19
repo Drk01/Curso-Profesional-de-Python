6d4ff74a6a3b9b352a2a8ab0053cec7492b38970
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-19 12:19:20 -0500

    Tipos de atributos en Python

---
+++ Clases/49.- Tipos de atributos.py
@@ -0,0 +1,16 @@
+class Circulo:
+    pi = 3.14159265  # Es una variable de clase
+
+    def __init__(self, radio):
+        self.radio = radio  # Es una variable de instancia
+
+
+circulo_a = Circulo(10)
+circulo_b = Circulo(20)
+
+circulo_b.radio = 100
+
+print(circulo_a.radio)
+print(circulo_b.radio)
+
+print(Circulo.pi)
