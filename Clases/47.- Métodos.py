090270b502f3a9e75ab931cf45155b901917f174
Omar Monrroy <ogcm.kael@hotmail.com>
2020-07-19 04:09:57 -0500

    Métodos en Python

---
+++ Clases/47.- Métodos.py
@@ -0,0 +1,11 @@
+class Usuario:
+
+    def saluda(self, nombre):
+        print('Hola, soy un usuario ' + nombre)
+
+
+codi = Usuario()
+facilito = Usuario()
+
+codi.saluda('Cody')
+facilito.saluda('Facilito')
